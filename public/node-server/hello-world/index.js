1390fddf025e21ac3f67a30744f5f6c537484ba6
Marco Aurélio <marcoaure47@gmail.com>
2018-01-25 17:53:17 -0200

    adding debuger on npm run dev in hello-world node-server

--- public/node-server/hello-world/index.js
+++ public/node-server/hello-world/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
+	abc(2,4);
   res.send('Hello World!');
 });
 
@@ -8,3 +9,11 @@ const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
 
 app.listen(PORT, HOST);
+
+
+function abc(a,b) {
+	c = a + b;
+	d = c + a;
+	e = d + b;
+	return e;
+}
